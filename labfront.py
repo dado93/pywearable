8e407d051ba79d018845546a70278ee1e78bd32d
Davide Marzorati <davide.marzorati.93@gmail.com>
2023-03-10 14:14:05 +0100

    Added functions for extraction of data from Labfront csv

--- labfront.py
+++ labfront.py
@@ -11,18 +11,39 @@ _LABFRONT_TODO_STRING = 'todo'
 BASE_FOLDER = Path('data') / 'sample_data' / \
     '9dbabd13-879e-4131-a562-66a2501435ab'
 
-ids, lf_ids = utils.get_ids(BASE_FOLDER)
+# Get dictionary with participant IDs
+id_dict = utils.get_ids(BASE_FOLDER, return_dict=True)
 
-id_dict = dict(zip(ids, lf_ids))
-
+# Select participant ID
 id = '01---ipad,-green-watch'
 labfront_id = id_dict[id]
 
+# Create folder name based on IDs
 path_to_data = id + '_' + labfront_id
-print(os.path.join(BASE_FOLDER, path_to_data))
-if not os.path.exists(os.path.join(BASE_FOLDER, path_to_data)):
+
+# Check that folder exists
+if not (BASE_FOLDER / path_to_data).exists():
     print(f'Data folder for participant {id} does not exist')
 
-for data_folder in os.listdir(os.path.join(BASE_FOLDER, path_to_data)):
-    if os.path.isdir(os.path.join(BASE_FOLDER, path_to_data, data_folder)):
-        print(os.path.join(BASE_FOLDER, path_to_data, data_folder))
+# This is the summary dictionary of the participant
+participant_summary_dict = {}
+
+for participant_data_folder in (BASE_FOLDER / path_to_data).iterdir():
+    # For each data folder of the participant
+    if participant_data_folder.is_dir():
+        # If it is a folder, then we need to read the csv files
+        for csv_file in participant_data_folder.iterdir():
+            if csv_file.is_file():
+                # Get header length
+                h_length = utils.get_header_length(csv_file)
+                print(h_length)
+                participant_summary_dict[participant_data_folder.name] = {}
+            elif csv_file.is_dir():
+                if _LABFRONT_QUESTIONNAIRE_STRIGN in csv_file:
+                    # We have questionnaires
+                    pass
+                elif _LABFRONT_TODO_STRING in csv_file:
+                    # We have todos
+                    pass
+
+print(participant_summary_dict)
--- utils.py
+++ utils.py
@@ -1,13 +1,18 @@
+
 import os
+import pandas as pd
 
 _LABFRONT_ID_LENGHT = 37
+_LABFRONT_FIRST_SAMPLE_UNIX_TIMESTAMP_IN_MS_KEY = 'firstSampleUnixTimestampInMs'
+_LABFRONT_LAST_SAMPLE_UNIX_TIMESTAMP_IN_MS_KEY = 'lastSampleUnixTimestampInMs'
+_LABFRONT_ISO_DATE_KEY = 'isoDate'
 
-
-def get_ids(folder):
+def get_ids(folder, return_dict=False):
     """Get participant IDs from folder with data.
 
     Args:
         folder (str): Path to folder containing data from participants.
+        return_dict (bool): Whether to return a dictionary or two lists. Defaults to False.
 
     Returns:
         list: IDs of participants
@@ -20,10 +25,50 @@ def get_ids(folder):
     for folder_name in folder_names:
         # Check that we have a folder
         if os.path.isdir(os.path.join(folder, folder_name)):
-            labfront_ids.append(folder_name[-_LABFRONT_ID_LENGHT+1:])
-            ids.append(folder_name[:(len(folder_name)-_LABFRONT_ID_LENGHT)])
-
+            labfront_id = folder_name[-_LABFRONT_ID_LENGHT+1:]
+            labfront_ids.append(labfront_id)
+            id = folder_name[:(len(folder_name)-_LABFRONT_ID_LENGHT)]
+            ids.append(id)
+    if return_dict:
+        return dict(zip(ids, labfront_ids))
     return ids, labfront_ids
+
+def get_header_length(file):
+    """Get header length of Labfront csv file.
+
+    Args:
+        file (str): Path to csv file.
+    """
+    # Read first line of file
+    with open(file, 'r') as f:
+        line = f.readline().split(',')
+    header_length = int(line[1])
+    return header_length
+
+def get_labfront_file_stats(path, header_length):
+    """_summary_
+
+    Args:
+        path (str): Path to the file from which stats have to be extracted
+        header_length (int): Length of the header in the csv file.
+
+    Returns:
+        int: First unix timestamp of data in csv file
+        int: Last unix timestamp of data in csv file
+        int: Sample rate (in seconds)
+    """
+
+    # Get first and last unix timestamps from header
+    header = pd.read_csv(path, nrows=1, skiprows=header_length-2)
+    first_unix_timestamp = header[_LABFRONT_FIRST_SAMPLE_UNIX_TIMESTAMP_IN_MS_KEY].iloc[0]
+    last_unix_timestamp = header[_LABFRONT_LAST_SAMPLE_UNIX_TIMESTAMP_IN_MS_KEY].iloc[0]
+    
+    # Get sample rate from data
+    data = pd.read_csv(path, nrows=2, skiprows=header_length+1, usecols=[_LABFRONT_ISO_DATE_KEY])
+    tmp_data = data[_LABFRONT_ISO_DATE_KEY]
+    sample_rate = (data[_LABFRONT_ISO_DATE_KEY].iloc[1] - data[_LABFRONT_ISO_DATE_KEY].iloc[0]).total_seconds()
+
+    return first_unix_timestamp, last_unix_timestamp, sample_rate
 
 
 def get_garmin_metrics_names(folder):
