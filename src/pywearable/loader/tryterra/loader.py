2f97900f834ff3b0ecd306c3b15e4b9d042f7bf0
Davide Marzorati <davide.marzorati.93@gmail.com>
2023-11-15 17:25:35 +0100

    Init function of TryTerraExportLoader

--- src/pywearable/loader/tryterra/loader.py
+++ src/pywearable/loader/tryterra/loader.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+from pathlib import Path
 from typing import Union
 
 import pandas as pd
@@ -6,15 +8,31 @@ import pandas as pd
 from ..base import BaseLoader
 
 
-class TryTerraLoader(BaseLoader):
-    def __init__(self, data_folder) -> None:
+class TryTerraExportLoader(BaseLoader):
+    def __init__(self, data_folder: Union[str, Path]) -> None:
         self.data_folder = data_folder
-        pass
+        try:
+            self.data_folder = Path(self.data_folder)
+            if not self.data_folder.is_dir():
+                raise ValueError(f"{self.data_folder} is not a folder.")
+        except:
+            raise ValueError(f"f{self.data_folder} is not a valid path.")
+        self.user_ids = [x.name for x in self.data_folder.iterdir() if x.is_dir()]
+
+        # Let's create a dictionary with information retrieved from the metadata
+        # field in the JSON files
+        # - start
+        # - end time stamp
+
+    def get_user_ids(self) -> list:
+        return self.user_ids
 
     def load_sleep_summary(
         self,
         user_id: str,
-        start_date: Union(datetime.datetime, datetime.date, str, None) = None,
-        end_date: Union(datetime.datetime, datetime.date, str, None) = None,
+        start_date: Union[datetime.datetime, datetime.date, str, None] = None,
+        end_date: Union[datetime.datetime, datetime.date, str, None] = None,
+        same_day_filter: bool = True,
     ) -> pd.DataFrame:
+        # TODO add check on user-id
         pass
