cc61222973daa971dd830ae28ec832aaf0d0fbf0
Davide Marzorati <davide.marzorati.93@gmail.com>
2024-01-24 16:35:10 +0100

    Added load_respiration base function

--- src/pywearable/loader/base.py
+++ src/pywearable/loader/base.py
@@ -7,6 +7,9 @@ import pandas as pd
 class BaseLoader:
     """Abstract class for the implementation of loader classes."""
 
+    def __init__(self):
+        pass
+
     def load_daily_summary(
         self,
         user_id: str,
@@ -45,6 +48,39 @@ class BaseLoader:
         start_date: Union[datetime.datetime, datetime.date, str, None] = None,
         end_date: Union[datetime.datetime, datetime.date, str, None] = None,
     ) -> pd.DataFrame:
+        raise NotImplementedError
+
+    def load_respiration(
+        self,
+        user_id: str,
+        start_date: Union[datetime.datetime, datetime.date, str, None] = None,
+        end_date: Union[datetime.datetime, datetime.date, str, None] = None,
+    ) -> pd.DataFrame:
+        """Load respiration data.
+
+        This function loads respiration data from
+        a given user and for a given time frame, from
+        ``start_date`` to ``end_date``.
+
+        Parameters
+        ----------
+        user_id : str
+            User id for which data have to be loaded.
+        start_date : datetime.datetime or datetime.date or str or None, optional
+            Start date for data retrieval, by default None
+        end_date : datetime.datetime or datetime.date or str or None, optional
+            End date for data retrieval, by default None
+
+        Returns
+        -------
+        :class:`pd.DataFrame`
+            DataFrame with respiration data.
+
+        Raises
+        ------
+        NotImplementedError
+            Method must be implement by loaders.
+        """
         raise NotImplementedError
 
     def load_sleep_pulse_ox(
